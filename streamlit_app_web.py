bfe1f377d043ec0d6972db366fa7b1d2e129cd77
Erick <erick_sant_m@yahoo.com.mx>
2023-03-08 15:03:15 -0600

    committing files

--- streamlit_app_web.py
+++ streamlit_app_web.py
@@ -965,8 +965,8 @@ else:
     fechas = ", ".join(["'%s'" % str(d)[:10] for d in pd.date_range("2020-01-31", periods=150, freq="M")])
     _query = " and ".join([f for f in filtro_BQ.split(" and ") if "Fecha_reporte" not in f])
     df_cosechas = (BQ
-    #               .assign(Dias_de_atraso = lambda df: df.Dias_de_atraso.apply(lambda x: max(x, 0)))
-                   .query("Dias_de_atraso >= %i" % dias)
+                   .assign(Dias_de_atraso = lambda df: df.Dias_de_atraso.apply(lambda x: max(x, 0)))
+    #               .query("Dias_de_atraso >= %i" % dias)
                    .query("%s and Fecha_reporte in (%s)" 
                           % (_query, fechas)
                           )
