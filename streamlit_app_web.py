3215c58ea23c75f88af9fa35e6fdec8e62c22257
Erick <erick_sant_m@yahoo.com.mx>
2023-03-10 16:01:20 -0600

    committing files

--- streamlit_app_web.py
+++ streamlit_app_web.py
@@ -806,17 +806,13 @@ else:
                 fig0.layout.yaxis.tickprefix = '$'
         
     else:
-        fig0 = {
-                'data': [go.Bar(x=_to_plot0[factor]
-                                , y=_to_plot0[_kpi["y"]+"_avg"]
-                               )
-                         , go.Bar(x=_to_plot0[factor]
+        fig0 = go.Figure()
+        fig0.add_trace(go.Bar(x=_to_plot0[factor], y=_to_plot0[_kpi["y"]+"_avg"]))
+        fig0.add_trace(go.Bar(x=_to_plot0[factor]
                                   , y=_to_plot0[_kpi["y"]]
                                   , width=len(_to_plot0)*[0.5]
-                                  )
-                         ]
-                , 'layout': go.Layout(barmode='overlay')
-            } 
+                                  ))
+        fig0.update_layout(barmode = 'overlay')
         fig0.layout.yaxis.tickformat = ',.1%'
         fig0.layout.yaxis.range = [0, 1]
 
