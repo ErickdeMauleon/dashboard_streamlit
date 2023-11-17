cf98b524fe2394f32158c791e2c764a4fbdc529a
Erick <erick_sant_m@yahoo.com.mx>
2023-11-17 15:27:02 -0700

    Update Propuesta clusters.py

--- pages/Propuesta clusters.py
+++ pages/Propuesta clusters.py
@@ -2,7 +2,6 @@ import geopandas as gpd
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# import geopandas as gpd
 import plotly.express as px
 import socket
 import streamlit as st
@@ -253,5 +252,86 @@ x0 = (creditos
     )
 st.write(len(x0))
 st.markdown("Estos son los créditos que están fuera de la zona")
-st.dataframe(x0, height=500)
+st.table(x0, height=500)
+
+colonias["zona"] = "Fuera de zona"
+
+fig, ax = plt.subplots(1, 2, figsize=(20, 15))
+
+for i in [0,1]:
+    zonas = ("Texcoco", "Nezahualcoyotl", "Iztapalapa 1", "Cuautitlan") if i == 0 else ("Puebla", "")
+    colores = ("red", "blue", "green", "orange") if i == 0 else ("purple", "")
+    for _zona, _color in zip(zonas, colores):
+        poligono = (poligonos
+                    .query("zona == '%s'" % _zona.replace(" ", "_"))
+                    )
+        if len(poligono) > 0:
+            poligono.plot(ax=ax[i]
+                    , color=_color
+                    , alpha=0.3
+                    , label=_zona
+                )
+            colonias.loc[colonias.within(poligono.dissolve(by="zona").geometry.iloc[0]), "zona"] = _zona
+
+    for _zona, _color in zip(zonas, colores):
+        colonias.query("zona == '%s'" % _zona).plot(ax=ax[i], color=_color, alpha=1, markersize=10, label=_zona)
+
+    colonias.query("zona == 'Fuera de zona'").plot(ax=ax[i], color="lightgrey", alpha=0.35, markersize=10, label="Fuera de zona")
+            
+
+        
+    permitidos = "NOMEDO.isin(['D.F.', 'Mexico', 'Puebla', 'Tlaxcala'])"
+    mpos.query(permitidos).boundary.plot(lw=1, color='lightgrey', ax=ax[i], alpha=0.4)
+    mx.query(permitidos).boundary.plot(lw=1, color='k', ax=ax[i])
+    poligonos.plot(ax=ax[i], color="lightgrey", alpha=0.5)
+    l, u = (-99.5, -98.6) if i == 0 else (-98.4, -98)
+    ax[i].set_xbound(lower=l, upper=u)
+    l, u = (19, 20) if i == 0 else (18.9, 19.3)
+    ax[i].set_ybound(lower=l, upper=u)
+    ax[i].legend(fontsize=10)
+
+# Make marker bigger in legend box
+leg = ax[0].get_legend()
+leg.legendHandles[0]._sizes = [100]
+leg.legendHandles[1]._sizes = [100]
+leg.legendHandles[2]._sizes = [100]
+leg.legendHandles[3]._sizes = [100]
+# Set legend box position in upper right corner
+leg.set_bbox_to_anchor((0.85, 1))
+
+
+leg = ax[1].get_legend()
+leg.legendHandles[0]._sizes = [100]
+
+st.pyplot(fig, use_container_width=True, clear_figure=True)
+
+x1 = (colonias
+    .filter(["zip_code", "latitud", "longitud", "zona"])
+    .merge(pd.read_csv("https://raw.githubusercontent.com/ErickdeMauleon/data/main/Sepomex.csv"
+                        , usecols=["d_codigo", "D_mnpio", "d_estado"]
+                        , dtype={"d_codigo": str}
+                        )
+            .drop_duplicates(subset=["d_codigo"], keep="first")
+            .rename(columns={"d_codigo": "zip_code", "D_mnpio": "municipio", "d_estado": "estado"})
+            , on="zip_code"
+            )
+    )
+
+
+st.markdown("Estos son los créditos que están fuera de la zona o dentro de la zona")
+
+
+csv = x1.to_csv(index=False).encode('utf-8')
+
+_, _, _, d = st.columns(4)
+d.download_button(
+    label="Descargar CSV",
+    data=csv,
+    file_name='codigos.csv',
+    mime='text/csv',
+)
+st.table(x1.head(15), height=500)
+
+
+
 
