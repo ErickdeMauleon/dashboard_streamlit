fab066a6456950ec1358ae6f38944e67ed9a1d82
Erick <erick_sant_m@yahoo.com.mx>
2024-04-02 18:01:46 -0600

    committing files

--- streamlit_app_web.py
+++ streamlit_app_web.py
@@ -1570,7 +1570,7 @@ else:
             , "Cuentas (sin castigo)": {"y": "account_id", "query": "and Dias_de_atraso < 120"}
             , "Cuentas (castigadas)": {"y": "account_id", "query": "and Dias_de_atraso >= 120"}
             , "Cuentas activas": {"y": "account_id", "query": "and Status_credito != 'I' and Dias_de_atraso < 120"}
-            , "Cuentas inactivas": {"y": "account_id", "query": "and Status_credito == 'I' or Dias_de_atraso >= 120"}
+            , "Cuentas inactivas": {"y": "account_id", "query": "and (Status_credito == 'I' or Dias_de_atraso >= 120)"}
             , "Cuentas bloqueadas": {"y": "account_id", "query": "and allow_disbursements == 0 and Status_credito == 'I' and Dias_de_atraso < 120"}
             , "Saldo Total": {"y": "balance", "query": ""}
             , "Saldo Total (sin castigos)": {"y": "balance", "query": "and Dias_de_atraso < 120"}
